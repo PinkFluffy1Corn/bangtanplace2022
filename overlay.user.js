a5b5d85e2ee92e97a6e9932146a73c5d2c1b15f5
Linde Van Wonterghem <linde.vanwonterghem@klasse.be>
2023-07-20 18:54:55 +0200

    Adjust script urls and add temporary overlay

--- overlay.user.js
+++ overlay.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name         BTS r/place overlay
+// @name         r/place 2023
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  makes it easier to correctly place tiless
-// @author       PinkFluffy1Corn
-// @match        https://hot-potato.reddit.com/embed*
+// @description  try to take over the world!
+// @author       You
+// @match        *garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
 
 window.addEventListener('load', () => {
-        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+    console.log('r/place overlay')
+        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0].appendChild(
     (function () {
         const i = document.createElement("img");
-        i.src = "https://github.com/PinkFluffy1Corn/bangtanplace2022/raw/main/overlay.png";
-        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-        console.log(i);
+        i.src = "https://raw.githubusercontent.com/PinkFluffy1Corn/bangtanplace2022/2023/overlay-v2.png";
+        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
         return i;
     })())
 }, false);
